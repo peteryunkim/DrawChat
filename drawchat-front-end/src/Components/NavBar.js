66a3b18eb95979460bf88dcfa62f1051934e8f41
peteryunkim <peteryunkim@gmail.com>
2017-06-15 17:26:34 -0400

    fixed duplicate classname on element in navbar

--- drawchat-front-end/src/Components/NavBar.js
+++ drawchat-front-end/src/Components/NavBar.js
@@ -13,7 +13,7 @@ class NavBar extends React.Component{
 		return(
 			<nav className="navbar navbar-default">
 				<div>
-		      <img className='navbar-brand'alt='DrawChat!'className='logo' src='./sketch%20chat-03.png' width='150px'/>
+		      <img alt='DrawChat!'className='logo' src='./sketch%20chat-03.png' width='150px'/>
 		    </div>
 					<button id='logout-button' className="btn btn-default btn-md" onClick={() =>this.handleLogout()}>Logout</button>
 			</nav>
