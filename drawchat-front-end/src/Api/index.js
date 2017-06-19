5bab34f28d60ffbd5cd08ea492bcfead1702f6a5
peteryunkim <peteryunkim@gmail.com>
2017-06-19 11:33:54 -0400

    fixed websockets so it works on multiple computers

--- drawchat-front-end/src/Api/index.js
+++ drawchat-front-end/src/Api/index.js
@@ -2,40 +2,40 @@ import axios from 'axios'
 
 //user account
 export function createUser(user){
-	return axios.post("http://localhost:3000/api/v1/users", user)	
+	return axios.post(`http://${window.location.hostname}:3000/api/v1/users`, user)	
 }
 
 export function signIn(user){
-	return axios.post("http://localhost:3000/api/v1/auth", user)	
+	return axios.post(`http://${window.location.hostname}:3000/api/v1/auth`, user)	
 }		
 
 export function getUser(token){
-	return axios.post("http://localhost:3000/api/v1/user", {Authorization: token})
+	return axios.post(`http://${window.location.hostname}:3000/api/v1/user`, {Authorization: token})
 }
 
 //chatroom
 export function getMessages(){
-	return axios.get("http://localhost:3000/api/v1/messages")
+	return axios.get(`http://${window.location.hostname}:3000/api/v1/messages`)
 }
 export function postMessage(message, userId){
-	return axios.post("http://localhost:3000/api/v1/messages", {message, userId})
+	return axios.post(`http://${window.location.hostname}:3000/api/v1/messages`, {message, userId})
 }
 
 
 //canvas
 export function getDrawings(){
-	return axios.get("http://localhost:3000/api/v1/canvas")
+	return axios.get(`http://${window.location.hostname}:3000/api/v1/canvas`)
 } //populate list of canvas 
 
 export function saveDrawingDB(canvasUrl,canvasName){
-	return axios.post("http://localhost:3000/api/v1/canvas", {canvasUrl,canvasName})
+	return axios.post(`http://${window.location.hostname}:3000/api/v1/canvas`, {canvasUrl,canvasName})
 }
 
 export function selectExistingDrawing(canvas){
-	return axios.post("http://localhost:3000/api/v1/one_canvas", {canvas: canvas})
+	return axios.post(`http://${window.location.hostname}:3000/api/v1/one_canvas`, {canvas: canvas})
 }
 
 export function deleteCanvas(canvas){
-	return axios({method: 'delete', url:"http://localhost:3000/api/v1/canvas" , data: {canvas}} ) 
+	return axios({method: 'delete', url:`http://${window.location.hostname}:3000/api/v1/canvas` , data: {canvas}} ) 
 }
 
