b1de07951eaf8db4a32c6df804e840b2e8cf065c
peteryunkim <peteryunkim@gmail.com>
2017-06-22 09:16:25 -0400

    changed logo and fixed css to accomdate logo

--- drawchat-front-end/src/Components/NavBar.js
+++ drawchat-front-end/src/Components/NavBar.js
@@ -14,7 +14,7 @@ class NavBar extends React.Component{
 		return(
 			<nav className="navbar navbar-default">
 				<div>
-		      <img alt='DrawChat!'className='logo' src='./sketch%20chat-03.png' width='150px'/>
+		      <img alt='DrawChat!'className='logo' src='./sketch%20chat%20V2-04.png' height="78px" width="350px"/>
 		      <CustomBackground />
 		    </div>
 					<button id='logout-button' className="btn btn-default btn-md" onClick={() =>this.handleLogout()}>Logout</button>
